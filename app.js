f82866cabc81dd6af86b56c56f01829a94004e1b
Lesley Kimutai <leskimuti@gmail.com>
2021-05-09 16:56:33 +0300

    First Commit Mum n Lesley Add

---
+++ app.js
@@ -0,0 +1,43 @@
+// jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const _ = require('lodash');
+
+
+const app = express();
+
+app.set ('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+
+const year = new Date().getFullYear();
+// console.log(year);
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/mum", function(req, res){
+  res.render("mum", {thisYear: year} );
+});
+
+app.get("/lesley", function(req, res){
+  res.render("lesley", {thisYear: year});
+});
+
+app.get("/kibenon", function(req, res){
+
+});
+
+app.get("/kibiwot", function(req, res){
+
+});
+
+
+app.listen(3000, function(){
+  console.log("Server started om port 3000");
+});
