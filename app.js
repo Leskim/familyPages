55408a1e6be732bd3f879b9724fce756dbd3dd4f
Lesley Kimutai <leskimuti@gmail.com>
2021-05-09 19:18:57 +0300

    Add gitignore and engine for node

--- app.js
+++ app.js
@@ -37,7 +37,11 @@ app.get("/kibiwot", function(req, res){
 
 });
 
+let port = process.env.PORT;
+if (port == null || port == "" ){
+  port = 3000;
+}
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen( port, function(){
   console.log("Server started om port 3000");
 });
