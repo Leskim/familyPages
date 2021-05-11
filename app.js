506552a63565720eb938872ebfd83e2f683d0bc9
Lesley Kimutai <leskimuti@gmail.com>
2021-05-11 11:10:17 +0300

    Addition of friends page,  kibi and kibenon

--- app.js
+++ app.js
@@ -8,40 +8,56 @@ const _ = require('lodash');
 
 const app = express();
 
-app.set ('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('public'));
 
 
 const year = new Date().getFullYear();
 // console.log(year);
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/mum", function(req, res){
-  res.render("mum", {thisYear: year} );
+app.get("/mum", function(req, res) {
+  res.render("mum", {
+    thisYear: year
+  });
 });
 
-app.get("/lesley", function(req, res){
-  res.render("lesley", {thisYear: year});
+app.get("/lesley", function(req, res) {
+  res.render("lesley", {
+    thisYear: year
+  });
 });
 
-app.get("/kibenon", function(req, res){
-
+app.get("/friends", function(req, res) {
+  res.render("friends", {
+    thisYear: year
+  });
 });
 
-app.get("/kibiwot", function(req, res){
+app.get("/kibenon", function(req, res) {
+  res.render("kibenon", {
+    thisYear: year
+  });
+});
 
+app.get("/kibiwot", function(req, res) {
+  res.render("kibi", {
+    thisYear: year
+  });
 });
 
 let port = process.env.PORT;
-if (port == null || port == "" ){
+if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen( port, function(){
+app.listen(port, function() {
   console.log("Server started om port 3000");
 });
